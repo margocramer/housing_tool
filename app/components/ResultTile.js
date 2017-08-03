4d5babb8220b7e07ab8527ae2230a72b3c935e03
Marguerite Cramer <margueritecramer@Marguerites-MacBook-Air.local>
2017-08-03 13:38:45 -0400

    renders remote image from web

--- app/components/ResultTile.js
+++ app/components/ResultTile.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ResultTile = (props) => {
   return(
-    <h1>Result!</h1>
+    <div className="result">
+      <img src='http://demandware.edgesuite.net/sits_pod32/dw/image/v2/BBBW_PRD/on/demandware.static/-/Sites-jss-master/default/dw625729c3/images/products/flowers/01712_01_procutorange.jpg?sw=1120'/>
+    </div>
   )
 }
 
--- public/bundle.js
+++ public/bundle.js
@@ -20199,9 +20199,9 @@
 
 	var ResultTile = function ResultTile(props) {
 	  return _react2.default.createElement(
-	    'h1',
-	    null,
-	    'Result!'
+	    'div',
+	    { className: 'result' },
+	    _react2.default.createElement('img', { src: 'http://demandware.edgesuite.net/sits_pod32/dw/image/v2/BBBW_PRD/on/demandware.static/-/Sites-jss-master/default/dw625729c3/images/products/flowers/01712_01_procutorange.jpg?sw=1120' })
 	  );
 	};
 
